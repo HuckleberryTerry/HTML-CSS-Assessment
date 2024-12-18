1ef04f7cd1cac1dbfb7419cc12b0f497ad3a388c
HuckleberryTerry <jozef.jenkins19@gmail.com>
2024-12-18 16:37:38 +0000

    EOD Push
    I have forgotten to push for a few days,
    
    This update covers the following:
    
    Header:
    -Fixed the container so that it is the correct size
    -added multiple styles for different viewports
    -fixed breakpoints for viewports
    -Added in the hoverable menu for larger screen sizes
    
    Banner:
    -Added in images
    -styled the text text to change on screen size
    -re-factored the slick-dots to look more centered on active style
    
    Service Cards:
    -Changed container classes to make the padding correct
    -Changed the layout on larger screen sizes
    -made the smaller layouts more accurate
    
    Welcome Section:
    -Removed padding from inner classes that was pushing content out of line
    -Re-styled the buttons to be closer to the original webpages
    -re-factored the layout style to better emulate the original webpages
    
    Article Cards:
    -Refactored to the same standards as other sections, removing unnecessary padding and margins
    
    SideBar:
    -Styled the SideBar to look similar to the one on the netmatters homepage
    -Changed the JS to better change the layout responsively
    
    Carousels:
    -Attempted to create a carousel that shows text boxes on hover for the clients carousel. Has not been implemented and does not currently work

--- js/main.js
+++ js/main.js
@@ -20,17 +20,30 @@ $(document).ready(function(){
         pauseOnFocus:       true,
         autoplay:           true
     });
-    $('.clients-banner').slick({
+    $('.description').slick({
         arrows:             false,
         dots:               false,
-        autoplay:           true,
+        //autoplay:           true,
         slidesToShow:       3,
         slidesToScroll:     1,
         infinite:           true,
-        centerMode:         true,
         variableWidth:      true,
         swipe:              false,
-        draggable:          false
+        draggable:          false,
+        centerMode:         true
+    });
+    $('.clients-banner').slick({
+        slide:              '.client',
+        arrows:             false,
+        dots:               false,
+        //autoplay:           true,
+        slidesToShow:       3,
+        slidesToScroll:     1,
+        infinite:           true,
+        variableWidth:      true,
+        swipe:              false,
+        draggable:          false,
+        centerMode:         true
     });
     $('.partners').slick({
         arrows:             false,
@@ -39,7 +52,6 @@ $(document).ready(function(){
         slidesToShow:       3,
         slidesToScroll:     1,
         infinite:           true,
-        centerMode:         true,
         variableWidth:      true,
         swipe:              false,
         draggable:          false
@@ -69,10 +81,14 @@ $(document).ready(function(){
 window.onscroll = function(){
     let currentScrollPos = window.scrollY;
     console.log(currentScrollPos);
-    if(currentScrollPos < 400){
-        $('#header').removeAttr('class');
+    if(currentScrollPos <= 400 || prevScrollPos <= 400){
+        $('#header').removeClass('header-hidden');
+        $('#header').removeClass('header-visible');
+        $('#header').css({
+            'position': 'relative'
+        })
     }
-    else if(prevScrollPos < currentScrollPos){
+    else if(prevScrollPos < currentScrollPos && currentScrollPos > 400){
         $('#header').css({
             'position': 'sticky'
         })
@@ -97,14 +113,29 @@ window.onscroll = function(){
 //Currently works in practice, I would like to implement a version of this that uses
 //Promises or Async JS to ensure that the stack doesn't get clogged with JS Animation Calls.
 
+function sidebarAnimate(){
+    let $window_width = $(window).width();
+    let $width = $sideBar.width() * -1;
+    if($window_width <= 972){
+        $containerBody.css('left','-275px');
+    }
+    else{
+        //console.log($window_width);
+        $containerBody.css('left','-375px');
+    }
+}
+
+
 $hammedBurger.on('click', function(event){
+    let $width = $sideBar.width() * -1;
+    let $window_width = $(window).width();
     $sideBar.show();
     //$sideBar.animate({width: '275px'});
     $sideBar.addClass('active');
     $hammedBurger.addClass('open');
     //$containerBody.css('height', '100%');
     $containerBody.attr('id', 'side-lined');
-    $containerBody.animate({left:'-275px'}, 250);
+    $containerBody.animate({left:$width}, 250);
     $(document).on('click', function(event){
         if(event.target.id === 'side-lined'){
             //$sideBar.animate({width: '-275px'});
@@ -115,6 +146,15 @@ $hammedBurger.on('click', function(event){
             //$containerBody.css('height', '600px');
             $sideBar.removeClass('active');
             //console.log(event.target);
+        }
+    })
+
+    $(window).resize(function(){
+        if($containerBody.attr('id') === 'side-lined'){
+            sidebarAnimate();
+        }
+        else{
+            $sideBar.hide();
         }
     })
 });
@@ -141,6 +181,97 @@ $hammedBurger.on('click', function(event){
 //     })
 // });
 
+//Accreditations: Image Replacement Mouse-Enter
+const $accred_Child = $('.partner');
+$accred_Child.on('mouseenter', function(){
+    console.log($(this).data('slickIndex'));
+    let $accred_nthChild = $(this).data('slickIndex');
+    if($accred_nthChild === 1 || $accred_nthChild === 13){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/living_wage.png');
+    }
+    else if($accred_nthChild === 2 || $accred_nthChild === 14){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/norfolk_prohelp.png');
+    }
+    else if($accred_nthChild === 3 || $accred_nthChild === 15){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/investing-in-future-growth.jpg');
+    }
+    else if($accred_nthChild === 4 || $accred_nthChild === 16){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/norfolk-carbon-charter.jpg');
+    }
+    else if($accred_nthChild === 5 || $accred_nthChild === 17){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/PPC_logo.jpg');
+    }
+    else if($accred_nthChild === 6){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/princess-royal-training.png');
+    }
+    else if($accred_nthChild === 7){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/future-50.jpg');
+    }
+    else if($accred_nthChild === 8 || $accred_nthChild === -4){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/qms.png');
+    }
+    else if($accred_nthChild === 9 ||  $accred_nthChild === -3){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/iso-27001.png');
+    }
+    else if($accred_nthChild === 10 || $accred_nthChild === -2){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/skills-of-tomorrow.jpg');
+    }
+    else if($accred_nthChild === 11 || $accred_nthChild === -1){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/cyber-essentials-colour.jpg');
+    }
+    else if($accred_nthChild === 12 || $accred_nthChild === 0){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/google-partner.jpg');
+    }
+    else{
+        console.log('Something has gone wrong');
+    }
+});
+
+//Accreditations: Image Replacement Mouse-Leave
+
+$accred_Child.on('mouseleave', function(){
+    console.log($(this).data('slickIndex'));
+    let $accred_nthChild = $(this).data('slickIndex');
+    if($accred_nthChild === 1 || $accred_nthChild === 13){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/living_wage_bw.png');
+    }
+    else if($accred_nthChild === 2 || $accred_nthChild === 14){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/norfolk_prohelp_bw.png');
+    }
+    else if($accred_nthChild === 3 || $accred_nthChild === 15){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/investing-in-future-growth-bw.jpg');
+    }
+    else if($accred_nthChild === 4 || $accred_nthChild === 16){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/norfolk-carbon-charter-bw.jpg');
+    }
+    else if($accred_nthChild === 5 || $accred_nthChild === 17){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/PPC_logo-bw.jpg');
+    }
+    else if($accred_nthChild === 6){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/princess-royal-training-bw.png');
+    }
+    else if($accred_nthChild === 7){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/future-50-bw.jpg');
+    }
+    else if($accred_nthChild === 8 || $accred_nthChild === -4){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/qms-bw.png');
+    }
+    else if($accred_nthChild === 9 ||  $accred_nthChild === -3){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/iso-27001-bw.png');
+    }
+    else if($accred_nthChild === 10 || $accred_nthChild === -2){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/skills-of-tomorrow-bw.jpg');
+    }
+    else if($accred_nthChild === 11 || $accred_nthChild === -1){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/cyber-essentials-grey.jpg');
+    }
+    else if($accred_nthChild === 12 || $accred_nthChild === 0){
+        $(this).find('img').attr('src', 'assets/images/Accreditations/google-partner-bw.jpg');
+    }
+    else{
+        console.log('Something has gone wrong');
+    }
+});
 
 
 //Cookies: Check Accept
